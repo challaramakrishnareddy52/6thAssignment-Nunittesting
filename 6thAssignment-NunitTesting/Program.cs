fed44559d67be4f1bc7d5909d108a6982ec5a33e
challaramakrishnareddy52 <challaramakrishnareddy52@gmail.com>
2022-11-19 10:02:23 +0530

    Finding sq root

---
+++ 6thAssignment-NunitTesting/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6thAssignment_NUnitTesting
+{
+    internal class FindingSqRoot
+    {
+        public static void SquareRoot()
+        {
+            Console.WriteLine("enter a number to find its square root ");
+            double c = Convert.ToDouble(Console.ReadLine());    
+            Console.Write($"Square root of {c} is :");
+            double t = c;
+            double epsilon = 1.0e-15;                   
+
+            while (Math.Abs(t - c / t) > epsilon * t)   
+            {
+                t = (c / t + t) / 2.0;                  
+            }
+            Console.WriteLine(t);
+        }
+    }
+}
